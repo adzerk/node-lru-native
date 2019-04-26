a9d1ee575714721c20d3f898898cb61645d8f967
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2019-04-26 20:41:30 +0200

    :sparkles: apply prettier config and linter warnings

--- test/basics.tests.js
+++ test/basics.tests.js
@@ -1,67 +1,72 @@
-const assert = require('chai').assert;
-const LRUCache = require('../index.js');
+const assert = require("chai").assert
+const LRUCache = require("..")
 
-describe('basics', () => {
-  describe('a cache with default settings', () => {
-    let cache = undefined;
+describe("basics", () => {
+  describe("a cache with default settings", () => {
+    let cache = undefined
     beforeEach(() => {
-      cache = new LRUCache();
-    });
+      cache = new LRUCache()
+    })
 
-    it('should be able to set and retrieve primitive values', () => {
-      cache.set('foo', 42);
-      const value = cache.get('foo');
-      assert.equal(value, 42);
-    });
+    it("should be able to set and retrieve primitive values", () => {
+      cache.set("foo", 42)
+      const value = cache.get("foo")
+      assert.equal(value, 42)
+    })
 
-    it('should be able to set and retrieve string values', () => {
-      cache.set('foo', 'bar');
-      const value = cache.get('foo');
-      assert.equal(value, 'bar');
-    });
+    it("should be able to set and retrieve string values", () => {
+      cache.set("foo", "bar")
+      const value = cache.get("foo")
+      assert.equal(value, "bar")
+    })
 
-    it('should be able to set and retrieve object values', () => {
-      const obj = {example: 42}
-      cache.set('foo', obj);
-      const value = cache.get('foo');
-      //assert(value === obj, 'an unexpected item was returned');
-    });
+    it("should be able to set and retrieve object values", () => {
+      const obj = { example: 42 }
+      cache.set("foo", obj)
+      const value = cache.get("foo")
+      assert(value === obj, "an unexpected item was returned")
+    })
 
-    describe('when remove() is called', () => {
-      it('should remove the specified item', () => {
-        cache.set('foo', 42);
-        assert.equal(cache.get('foo'), 42);
-        cache.remove('foo');
-        const value = cache.get('foo');
-        assert(value === undefined, `expected result to be undefined, was #{value}`);
-      });
-    });
+    describe("when remove() is called", () => {
+      it("should remove the specified item", () => {
+        cache.set("foo", 42)
+        assert.equal(cache.get("foo"), 42)
+        cache.remove("foo")
+        const value = cache.get("foo")
+        assert(
+          value === undefined,
+          `expected result to be undefined, was #{value}`
+        )
+      })
+    })
 
-    describe('when size() is called', () => {
-      it('should return the correct size', () => {
-        assert.equal(cache.size(), 0);
-        cache.set('foo', 42);
-        assert.equal(cache.size(), 1);
-      });
-    });
+    describe("when size() is called", () => {
+      it("should return the correct size", () => {
+        assert.equal(cache.size(), 0)
+        cache.set("foo", 42)
+        assert.equal(cache.size(), 1)
+      })
+    })
 
-    describe('when clear() is called', () => {
-      it('should remove all items in the cache', () => {
-        cache.set('foo', 42);
-        cache.set('bar', 21);
-        cache.clear();
-        assert(cache.get('foo') === undefined);
-        assert(cache.get('bar') === undefined)
-      });
-    });
+    describe("when clear() is called", () => {
+      it("should remove all items in the cache", () => {
+        cache.set("foo", 42)
+        cache.set("bar", 21)
+        cache.clear()
+        assert(cache.get("foo") === undefined)
+        assert(cache.get("bar") === undefined)
+      })
+    })
 
-    describe('when stats() is called', () => {
-      it('should return fun statistics', () => {
-        cache.set('foo', 42);
-        const stats = cache.stats();
-        assert(stats.size == 1, `expected stats.size to be 1, was #{stats.size}`);
-      });
-    });
-  });
-});
-
+    describe("when stats() is called", () => {
+      it("should return fun statistics", () => {
+        cache.set("foo", 42)
+        const stats = cache.stats()
+        assert(
+          stats.size === 1,
+          `expected stats.size to be 1, was #{stats.size}`
+        )
+      })
+    })
+  })
+})
--- test/maxAge.tests.js
+++ test/maxAge.tests.js
@@ -1,30 +1,30 @@
-const assert = require('chai').assert;
-const LRUCache = require('..');
+const assert = require("chai").assert
+const LRUCache = require("..")
 
-describe('maxAge', () => {
-  describe('a cache with a maxAge of 100ms', () => {
-    let cache = undefined;
+describe("maxAge", () => {
+  describe("a cache with a maxAge of 100ms", () => {
+    let cache = undefined
     beforeEach(() => {
-      cache = new LRUCache({maxAge: 100});
-    });
+      cache = new LRUCache({ maxAge: 100 })
+    })
 
-    describe('when a value is requested less than 100ms after it is added', () => {
-      it('should return the value', () => {
-        cache.set('foo', 42);
-        value = cache.get('foo');
-        assert.equal(value, 42);
-      });
-    });
+    describe("when a value is requested less than 100ms after it is added", () => {
+      it("should return the value", () => {
+        cache.set("foo", 42)
+        value = cache.get("foo")
+        assert.equal(value, 42)
+      })
+    })
 
-    describe('when the value is requested more than 100ms after it is added', () => {
-      it('should return undefined', (done) => {
-        cache.set('foo', 42);
+    describe("when the value is requested more than 100ms after it is added", () => {
+      it("should return undefined", done => {
+        cache.set("foo", 42)
         setTimeout(() => {
-          value = cache.get('foo');
-          assert(value === undefined, `value was #{value}, expected undefined`);
-          done();
-        }, 200);
-      });
-    });
-  });
-});
+          value = cache.get("foo")
+          assert(value === undefined, `value was #{value}, expected undefined`)
+          done()
+        }, 200)
+      })
+    })
+  })
+})
